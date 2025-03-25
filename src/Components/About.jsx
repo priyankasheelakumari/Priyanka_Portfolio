40065ca1efe5a6133e58f2ee9ea4500a3c22be9c
priyankasheelakumari <128854026+priyankasheelakumari@users.noreply.github.com>
2025-03-25 13:19:19 -0400

    Added links for socialmedia accounts

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,25 +3,41 @@ import Box from "@mui/material/Box";
 import { Button, Typography, Link } from "@mui/material";
 import CV from "../assets/cv.pdf";
 import profileimage from "../assets/pro1.jpg";
-
+import linkedIn from "../assets/linkedin.svg";
+import github from "../assets/github.svg";
+import twitter from "../assets/twitter.svg"
 import { ReactTyped } from "react-typed";
 const About = () => {
   return (
-    <Box sx={{  display: "flex", flexDirection: { xs: "column", md: "row" }, alignItems: "center",justifyContent: "center",gap: { xs: 4, md: 10 }, 
-      textAlign: { xs: "center", md: "left" }, // Center text on mobile
-      px: { xs: 4, md: 10 }, // Responsive padding
-      py: { xs: 6, md: 10 },}}>
-      <Box sx={{ maxWidth: { xs: "80%", sm: "50%", md: "30%" }, flexShrink: 0 }}>
-    <img
-      className="pro-image"
-      src={profileimage}
-      alt="Profile"
-      style={{ width: "100%", height: "auto", borderRadius: "45%" }} 
-    />
-  </Box>
-      <Box sx={{  maxWidth: { xs: "90%", md: "50%" } }}>
-        <Typography variant="h4" sx={{ mb: 2 }}>Hello, I am </Typography>
-        <Typography variant="h2"sx={{ mb: 2, fontWeight: "bold" }}>Priyanka Sheelakumari</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", md: "row" },
+        alignItems: "center",
+        justifyContent: "center",
+        gap: { xs: 4, md: 10 },
+        textAlign: { xs: "center", md: "left" }, // Center text on mobile
+        px: { xs: 4, md: 10 }, // Responsive padding
+        py: { xs: 6, md: 10 },
+      }}
+    >
+      <Box
+        sx={{ maxWidth: { xs: "80%", sm: "50%", md: "30%" }, flexShrink: 0 }}
+      >
+        <img
+          className="pro-image"
+          src={profileimage}
+          alt="Profile"
+          style={{ width: "100%", height: "auto", borderRadius: "45%" }}
+        />
+      </Box>
+      <Box sx={{ maxWidth: { xs: "90%", md: "50%" } }}>
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          Hello, I am{" "}
+        </Typography>
+        <Typography variant="h2" sx={{ mb: 2, fontWeight: "bold" }}>
+          Priyanka Sheelakumari
+        </Typography>
         <Typography variant="h5" sx={{ mb: 4 }}>
           <ReactTyped
             strings={[
@@ -35,27 +51,55 @@ const About = () => {
             loop
           />
         </Typography>
+        <Box sx={{display:"flex",gap:4, justifyContent:"center", alignItems:"center",flexWrap:"wrap",mt:2}}>
+          <Link
+            href="https://github.com/priyankasheelakumari"
+            underline="none"
+            variant="inherit"
+            target="_blank"
+            aria-label="click to view Github"
+          >
+            <img src={github} alt="Github account" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/priyankasheelakumari/"
+            underline="none"
+            variant="inherit"
+            target="_blank"
+            aria-label="click to LinkedIn"
+          >
+            <img src={linkedIn} alt="Linkedin account" />
+          </Link>
+          <Link
+            href="https://x.com/i/flow/login"
+            underline="none"
+            variant="inherit"
+            target="_blank"
+            aria-label="click to view Twitter"
+          >
+            <img src={twitter} alt="Twitter account" />
+          </Link>
+        </Box>
         <Link
-        href={CV}
-        underline="none"
-        variant="inherit"
-        target="_blank"
-        aria-label="click to view resume"
-      >
-        <Button
-          variant="contained"
-          sx={{
-            color: "white",
-            bgcolor: "#d500f9",
-            boxShadow: " 0 0 10px rgb(182, 154, 182) ",
-            
-          }}
+          href={CV}
+          underline="none"
+          variant="inherit"
+          target="_blank"
+          aria-label="click to view resume"
         >
-          Resume
-        </Button>
-      </Link>
+          <Button
+            variant="contained"
+            sx={{
+              color: "white",
+              bgcolor: "#d500f9",
+              boxShadow: " 0 0 10px rgb(182, 154, 182) ",
+              my:"2rem"
+            }}
+          >
+            Resume
+          </Button>
+        </Link>
       </Box>
-      
     </Box>
   );
 };
