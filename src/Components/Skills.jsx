cc72593d00a36e066aa540b13400e787bb3cd9cf
priyankasheelakumari <128854026+priyankasheelakumari@users.noreply.github.com>
2025-04-04 13:06:57 -0400

    updated projects

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import CssIcon from '@mui/icons-material/Css';
+import HtmlIcon from '@mui/icons-material/Html';
+
+
+const Skills = () => {
+  return (
+    <Box sx={{display:"flex", gap:"2vh",alignItems:"center",justifyContent:"center",mt:10, overflow: "hidden",py:"15rem" }}>
+        <JavascriptIcon sx={{border:'2px solid black',fontSize:"10vh"}}/>
+        <CssIcon  sx={{border:'2px solid black',fontSize:"10vh"}}/>
+        <HtmlIcon  sx={{border:'2px solid black',fontSize:"10vh"}}/>
+        
+    </Box>
+  )
+}
+export default Skills
